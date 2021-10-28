1fbe843dc536fdb5951ba10cf3af18f4ead65348
unknown <navyas211@gmail.com>
2021-10-28 23:32:55 +0530

    [NAVYA]ADDED: Welcome to AddressBook System Programs

---
+++ src/com/BridgeLabz/AddressBookSystems/WelcomePage.java
@@ -0,0 +1,10 @@
+package com.BridgeLabz.AddressBookSystems;
+
+public class WelcomePage {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to Address Book System Program");
+
+	}
+
+}
